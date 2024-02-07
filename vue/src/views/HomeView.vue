4a57189d1e928c396701ad15241d892bc0e7e6fe
Kelly Ng <kellyng19lover@gmail.com>
2024-02-07 10:15:29 -0500

    as
    dasd

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,13 +1,56 @@
 <template>
   <div>
-<h1></h1>
+    <h1>HI</h1>
   </div>
 </template>
 
 <script setup>
-import DestCard from "@/components/DestCard.vue";
+
+const bowl= "https://banner2.cleanpng.com/20180411/cde/kisspng-ice-cream-sundae-dessert-mississippi-mud-pie-cup-cereal-bowl-5acd95ee896290.2300234915234227025627.jpg"
+const items= [
+  {
+    name: "strawberry icecream",
+    price: "$3",
+    category: "ice cream",
+    pic:"https://static.wixstatic.com/media/b2b081_7284a33242104ed28953e4ea0760a997~mv2.png/v1/fill/w_371,h_260,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/strawberry_fields_edited.png",
+  },
+  {
+    name: "vanilla icecream",
+    price: "$3",
+    category: "ice cream",
+    pic:"https://static.wixstatic.com/media/b2b081_d85873ddf9e9442aa509a469df59ef74~mv2.png/v1/fill/w_372,h_260,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/vanilla_volcano_edited.png",
+  },
+  {
+    name: "chocolate icecream",
+    price: "$3",
+    category: "ice cream",
+    pic:"https://static.wixstatic.com/media/b2b081_c7efff06b4e9466c804ba96bb449a3ff~mv2.png/v1/fill/w_338,h_237,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/award_winning_chocolate_edited.png",
+  },
+  {
+    name: "mint chocolate chip icecream",
+    price: "$3",
+    category: "ice cream",
+    pic:"https://static.wixstatic.com/media/b2b081_0ae2ff018c5646bba2c7bab97733d288~mv2.png/v1/fill/w_369,h_242,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/minty_mint_chip_edited.png",
+  },
+  {
+    name: "cookie dough icecream",
+    price: "$3",
+    category: "ice cream",
+    pic:"https://static.wixstatic.com/media/b2b081_4ac5214d684d426e99ee24c12c6a7485~mv2.png/v1/fill/w_392,h_273,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cookie_doughlicious_edited.png",
+  },
+  {
+    name: "whip cream",
+    price: "$1",
+    category: "toppings",
+    pic: "",
+  }
+  {
+    name: "",
+    price: "",
+    category: "",
+    pic: "", 
+  }
+]
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
